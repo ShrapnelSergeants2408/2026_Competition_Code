74180365ffd1f4cbd2f30e7f83c4570cd8228152
nimrod <nimrodium@protonmail.com>
2026-01-21 17:31:03 -0600

    autoformatter cleanup of subsystems

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -3,5 +3,6 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Climber extends SubsystemBase {
-    public Climber(){}
+
+    public Climber() {}
 }
--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,14 +1,14 @@
 package frc.robot.subsystems;
 
+import static frc.robot.Constants.ShooterConstants.*;
+
 import com.revrobotics.PersistMode;
 import com.revrobotics.ResetMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.SparkMaxConfig;
-
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import static frc.robot.Constants.ShooterConstants.*;
 
 public class Shooter extends SubsystemBase {
 
@@ -16,33 +16,39 @@ public class Shooter extends SubsystemBase {
     private final SparkMax shooterMotor;
     private final SparkMax feederMotor;
 
-    // CAN IDs 
+    // CAN IDs
     private static final int SHOOTER_MOTOR_ID = 1;
     private static final int FEEDER_MOTOR_ID = 2;
 
     // Motor speeds
     private static final double SHOOTER_SPEED = 0.9; // Shoots the ball
-    private static final double FEEDER_SPEED = 0.6;  // Feeds ball into shooter
+    private static final double FEEDER_SPEED = 0.6; // Feeds ball into shooter
 
     /**
-         * @param nominalVoltage 
-         * 
-         */
-        public Shooter(int nominalVoltage) {
-            shooterMotor = new SparkMax(SHOOTER_MOTOR_ID, MotorType.kBrushless);
-            feederMotor = new SparkMax(FEEDER_MOTOR_ID, MotorType.kBrushless);
-    
-    
-            // Optional safety limits
-            SparkMaxConfig config = new SparkMaxConfig();
-            config.voltageCompensation(12);
-            config.smartCurrentLimit(STALL_LIMIT);
-            
-            config.idleMode(IdleMode.kBrake);
-            shooterMotor.configure(config, ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters);
-            feederMotor.configure(config, ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters);
-    }
+     * @param nominalVoltage
+     *
+     */
+    public Shooter(int nominalVoltage) {
+        shooterMotor = new SparkMax(SHOOTER_MOTOR_ID, MotorType.kBrushless);
+        feederMotor = new SparkMax(FEEDER_MOTOR_ID, MotorType.kBrushless);
 
+        // Optional safety limits
+        SparkMaxConfig config = new SparkMaxConfig();
+        config.voltageCompensation(12);
+        config.smartCurrentLimit(STALL_LIMIT);
+
+        config.idleMode(IdleMode.kBrake);
+        shooterMotor.configure(
+            config,
+            ResetMode.kResetSafeParameters,
+            PersistMode.kNoPersistParameters
+        );
+        feederMotor.configure(
+            config,
+            ResetMode.kResetSafeParameters,
+            PersistMode.kNoPersistParameters
+        );
+    }
 
     /** Spins the shooter wheel to launch the ball */
     public void startShooter() {
