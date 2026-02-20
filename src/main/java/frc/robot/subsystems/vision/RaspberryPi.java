19a83ac96859e06fc7173a3c0755d42102cb7bdd
kyle willmann <kylewillmann@gmail.com>
2026-02-20 16:50:49 -0600

    defining raspi camera using getAllUnreadResults since getLatestResult is deprecated

--- src/main/java/frc/robot/subsystems/vision/RaspberryPi.java
+++ src/main/java/frc/robot/subsystems/vision/RaspberryPi.java
@@ -1,4 +1,41 @@
 package frc.robot.subsystems.vision;
+
+import java.lang.StackWalker.Option;
+import java.util.List;
+import java.util.Optional;
+
+import org.photonvision.PhotonCamera;
+import org.photonvision.targeting.PhotonTrackedTarget;
+
 class RaspberryPi {
-    
+    // record TargetPair(PhotonTrackedTarget target,double ambiguity){}
+    private PhotonCamera camera;
+    private double lastTimestamp;
+    private double lastCheck;
+    RaspberryPi(){
+
+    }
+    void updateFreshness(double timestampSeconds){
+        double nowSeconds = (double) System.currentTimeMillis()/1000;
+        if (this.lastTimestamp < timestampSeconds){
+            this.lastTimestamp = timestampSeconds;
+            this.lastCheck = nowSeconds;
+        }
+    }
+    Optional<PhotonTrackedTarget> getBestTarget(){
+        // if (this.camera.getAllUnreadResults().hasTargets()){
+        //     var target = this.camera.getAllUnreadResults().getBestTarget();
+        //     return Optional.of(
+        //         target
+        //         );
+        // } else return Optional.empty();
+        var results = this.camera.getAllUnreadResults();
+        List<Double> ambiguity = results.stream().map(r -> r.getPoseAmbiguity()).mapToDouble(Double::valueOf).toList();
+    }
+    List<Integer> getVisibleTags(){
+        var results = this.camera.getAllUnreadResults();
+        // for (var result:results){
+
+        // }
+    }
 }
