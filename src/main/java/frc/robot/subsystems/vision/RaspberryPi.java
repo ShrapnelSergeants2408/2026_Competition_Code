0d525752055e3b8bccd946dbedd21fc0f939c0a1
kyle willmann <kylewillmann@gmail.com>
2026-02-25 15:03:01 -0600

    made raspi constructor

--- src/main/java/frc/robot/subsystems/vision/RaspberryPi.java
+++ src/main/java/frc/robot/subsystems/vision/RaspberryPi.java
@@ -1,22 +1,39 @@
 package frc.robot.subsystems.vision;
 
-import java.lang.StackWalker.Option;
 import java.util.List;
 import java.util.Optional;
 
 import org.photonvision.PhotonCamera;
+import org.photonvision.PhotonPoseEstimator;
+import org.photonvision.PhotonPoseEstimator.PoseStrategy;
 import org.photonvision.targeting.PhotonTrackedTarget;
+
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.math.geometry.Transform3d;
 
 class RaspberryPi {
     // record TargetPair(PhotonTrackedTarget target,double ambiguity){}
+    
     private PhotonCamera camera;
+    private PhotonPoseEstimator estimator;
     private double lastTimestamp;
     private double lastCheck;
-    RaspberryPi(){
-
+    RaspberryPi(String cameraName, Transform3d cam /*what is this*/){
+        this.camera = new PhotonCamera(cameraName);
+        this.estimator = new PhotonPoseEstimator(
+            AprilTagFieldLayout.loadField(AprilTagFields.k2026RebuiltWelded), 
+            PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR,
+            cam
+        );
+        this.lastCheck = getNowSeconds();
+        this.lastTimestamp = -1.0;
+    }
+    double getNowSeconds(){
+        return (double) System.currentTimeMillis()/1000;
     }
     void updateFreshness(double timestampSeconds){
-        double nowSeconds = (double) System.currentTimeMillis()/1000;
+        double nowSeconds = getNowSeconds();
         if (this.lastTimestamp < timestampSeconds){
             this.lastTimestamp = timestampSeconds;
             this.lastCheck = nowSeconds;
@@ -29,13 +46,12 @@ class RaspberryPi {
         //         target
         //         );
         // } else return Optional.empty();
-        var results = this.camera.getAllUnreadResults();
-        List<Double> ambiguity = results.stream().map(r -> r.getPoseAmbiguity()).mapToDouble(Double::valueOf).toList();
+        return this.camera.getAllUnreadResults().stream().map(r -> r.getBestTarget()).reduce((acc,t) -> t.getPoseAmbiguity() < acc.getPoseAmbiguity() ? t : acc);        
     }
     List<Integer> getVisibleTags(){
-        var results = this.camera.getAllUnreadResults();
-        // for (var result:results){
-
-        // }
+        return this.camera.getAllUnreadResults().stream().flatMap(r -> r.getTargets().stream().map(t -> t.getFiducialId())).toList();
     }
+
+    
+
 }
